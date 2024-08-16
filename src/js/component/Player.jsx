94643953c189e900d042e3d2fb970ba00d560c3d
Herbert7365 <161256945+Herbert7365@users.noreply.github.com>
2024-08-16 23:05:33 +0000

    Avance de Reproductor de audio como Spotify con React

---
+++ src/js/component/Player.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+
+// export const MediaPlayer = ({ currentSong}) => {
+//     console.log(currentSong)
+//     return (
+//         <div>
+//             <div>Tracker</div>
+//             <div>
+//                 <span>
+//                 <FontAwesomeIcon icon="fa-solid fa-play" />
+//                 </span>
+//             </div>
+
+
+//         </div>
+//     )
+// }
+
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,98 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [songs, setSongs] = useState([]);
+	const [currentSong, setCurrentSong]=useState()
+	const [urlSong, setUrlSong] = useState("")
+	const [currentSongIndex, setCurrentSongIndex] = useState(0);
+
+	const cancionActual = useRef();
+
+	const getSongs = () => {
+		fetch(`https://playground.4geeks.com/sound/songs`)
+		.then((response) => response.json())
+		.then((data)=>{
+			setSongs(data.songs);
+		})
+		.catch((error)=> console.log("Error al obtejner efectos de sonido", error));
+	};
+
+	// const handleSelectedSong = (element) => {
+	// 	setCurrentSong(element)
+	// }
+
+	const getUrl = (url) => {
+		setUrlSong(url)
+		cancionActual.current.src=(`https://playground.4geeks.com${url}`)
+		cancionActual.current.play();
+	};
+
+	useEffect(() => {
+		// Llama a la función al cargar el componente
+		getSongs();
+	  }, []);
+
+	// const fucnionEjemplo = () => {
+
+
+	// const playSongs =(url)=>{
+	// 	url.playSongs
+	// }
+	
+	const handleNextSong = () => {
+		setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+		getUrl(songs[currentSongIndex].url);
+	};
+	
+	const handlePrevSong = () => {
+		setCurrentSongIndex((prevIndex) => (prevIndex - 1 + songs.length) % songs.length);
+		getUrl(songs[currentSongIndex].url);
+	};
 	return (
+		
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Efectos de sonido disponibles:</h1>
+			{console.log(currentSong)}
+			<ul>
+				{songs.map((element,i) => (
+				<li key={i} onClick={()=>getUrl(element.url)}>{element.name}</li>
+				))}
+      		</ul>
+			<audio ref={cancionActual} >cancion actual</audio>
+			{/* <button onClick={fucnionEjemplo}>Play</button> */}
+
+			<section>
+				<div>
+					<span>
+					<i className="fa-solid fa-play"></i>
+					</span>
+				</div>
+				
+				<div>
+					<span>
+					<i className="fa-solid fa-pause"></i>
+					</span>
+				</div>
+				<div>
+					<span>
+					<i onClick={handleNextSong} className="fa-solid fa-square-caret-right"></i>
+					</span>
+				</div>
+				<div>
+					<span>
+					<i onClick={handlePrevSong} className="fa-solid fa-square-caret-left"></i>
+					</span>
+				</div>
+			</section>
+			
+
 		</div>
+		
+	
 	);
 };
 
