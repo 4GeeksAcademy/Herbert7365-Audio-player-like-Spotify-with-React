3d055eb65cec079ffe8c14557d2f65eb2619213d
Herbert7365 <161256945+Herbert7365@users.noreply.github.com>
2024-08-19 04:10:34 +0000

    Cambios en home.jsx index.css y se elimino Player.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,102 +1,104 @@
 import React, { useState, useEffect, useRef } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { MediaPlayer } from "./Player";
 //create your first component
 const Home = () => {
-	const [songs, setSongs] = useState([]);
-	const [currentSongIndex, setCurrentSongIndex] = useState(0);
-	const [urlSong, setUrlSong] = useState("")
 
-	const audioPlayer = useRef();
+	const [list, setList] = useState([]);
+	const [isPlaying, setIsPlaying ] = useState(false);
+	const [music, setMusic] = useState(0)
+	const audioPlayer = useRef(null);
 
-	const getSongs = () => {
-		fetch(`https://playground.4geeks.com/sound/songs`)
-		.then((response) => response.json())
-		.then((data)=>{
-			setSongs(data.songs);
-			console.log("Array de objetos de canciones",data.songs)
-		})
-		.catch((error)=> console.log("Error al buscar canciones", error));
-	};
-
-	// const handleSelectedSong = (element) => {
-	// 	setCurrentSong(element)
-	// }
-
-	const getUrl = (url, index) => {
-		setUrlSong(url)
-		audioPlayer.current.src=(`https://playground.4geeks.com${url}`)
+	function getSongs() {
+		fetch("https://playground.4geeks.com/sound/songs")
+		.then((response) => {
+			return response.json();
+		  })
+		  .then((data) => setList(data.songs))
+		  .catch((err) => console.log(err));
+		}
+	
+		  useEffect(() => {
+		  getSongs();
+		}, []);
+	
+	const  setSong = (link , i) => {
+		audioPlayer.current.src = `https://playground.4geeks.com${link}`
+		audioPlayer.current.play();		
+		setMusic(i)
+		if (!isPlaying){
+			play()	
+		}
+	}
+	 
+	const play = () => {
+		const prevValue = isPlaying
+		setIsPlaying(!prevValue);
+		if (!prevValue){
+			audioPlayer.current.play()}
+		else {
+			audioPlayer.current.pause();
+		}
+	}
+	  
+	const next = () => {
+		let id=music;
+		if (!isPlaying){
+			play()	
+		}
+		if (music < list.length -1 ) {
+			setMusic(music+1);
+			id++
+		} else { 
+			setMusic(0);
+			id=0
+		}
+		audioPlayer.current.src = `https://playground.4geeks.com${list[id].url}`
+		console.log(`que es esto ${audioPlayer.current.src}`)
 		audioPlayer.current.play();
-	};
-	
-	useEffect(() => {
-		// Llama a la función al cargar el componente
-		getSongs();
-	  }, []);
-
-	// const fucnionEjemplo = () => {
-
-
-	// const playSongs =(url)=>{
-	// 	url.playSongs
-	// }
-	
-	const handleNextSong = () => {
-		setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
-		getUrl(songs[currentSongIndex].url);
-	};
-
-	const handlePrevSong = () => {
-		setCurrentSongIndex((prevIndex) => (prevIndex - 1 + songs.length) % songs.length);
-		getUrl(songs[currentSongIndex].url);
-	};
-
-	const handleSongClick = (element, index) => {
-		getUrl(element.url, index);
-	  };
+		const element = document.getElementById(id);
+			element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+	}
+		  
+		 
+	const prev = () => {
+		let id=music
+		if (!isPlaying){
+			play()	
+		}
+		if (music > 0) {
+			setMusic(music-1);
+			id--
+		}else { 
+			setMusic(list.length-1);
+			id=list.length-1;
+		}
+		audioPlayer.current.src = `https://playground.4geeks.com${list[id].url}`
+		console.log(`Esto es al pulsar prev ${audioPlayer.current.src}`)
+		audioPlayer.current.play();
+		const element = document.getElementById(id);
+			element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+	}
 
 	return (
 		
-		<div className="container text-center overflow-auto">
-			<section >
-				<h1>Efectos de sonido disponibles:</h1>
-				
-				<ol className="list-group">
-					{songs.map((element,i) => (
-					<li key={i} id={i} value={i} onClick={()=>handleSongClick(element, i)} className={"list-group-item p-3 text-start" + (i===currentSongIndex && urlSong ? " active": "")}><span>{element.id} </span>{element.name}</li>
+		<div className="container">
+			<div className="listsongs mx-auto">
+				<ol className="list-group list-group-numbered overflow-auto">
+					{list.map((item, index) => (
+					<li key={index} id={index} value={index} className={`list-group-item p-3 text-start ${ index === music && isPlaying ? "active" : "" }`}
+						onClick={() => setSong(item.url, index)}>
+						{item.name}
+					</li>
 					))}
 				</ol>
-				{/* <audio ref={audioPlayer} ></audio> */}
-				{/* <button onClick={fucnionEjemplo}>Play</button> */}
-			</section>
-			<section>
-				<div>
-					<audio ref={audioPlayer}>
-						<source src={currentSongIndex} type="audio/mpeg"/>
-					</audio>
+				<audio ref={audioPlayer} src="#" type="audio.mp3" />
+				<div className="controls mx-auto text-center">
+					<i className="btn fa-solid fa-square-caret-left" onClick={prev} ></i>
+					<i className={`btn ${isPlaying ? "fa-solid fa-pause" : "fa-solid fa-play"}`} onClick={play}	></i>
+					<i className="btn fa-solid fa-square-caret-right" onClick={next} ></i>
 				</div>
-				<div>
-					<span>
-					<i onClick={() => audioPlayer.current.play()} className="fa-solid fa-play"></i><i onClick={() => audioPlayer.current.pause()} className="fa-solid fa-pause"></i>
-					</span>
-				</div>
-				<div>
-					<span>
-					<i onClick={handleNextSong} className="fa-solid fa-square-caret-right"></i>
-					</span>
-				</div>
-				<div>
-					<span>
-					<i onClick={handlePrevSong} className="fa-solid fa-square-caret-left"></i>
-					</span>
-				</div>
-			</section>
-			
-
+			</div>
 		</div>
-		
 	
 	);
 };
